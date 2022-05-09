2c36e31741b47490d9d345b2ccb87bf9613c583a
catalin.festila <catafest76@gmail.com>
2022-05-09 11:38:12 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="wrapper">
+  <input v-model="item_title">
+  <button @click="addItem">Add</button>
+  <!-- ':' for bind and '@' for event -->
+  <ListItem 
+    v-for="(item, index) in item_list" 
+    :key="index" 
+    :title="item"
+    @delete-item="delItem(index)" 
+  />
+  <button @click="greet">Greet</button>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import ListItem from './components/List_Item.vue'
+  export default {
+    components: {
+      ListItem
+    },
+
+    data () {
+        return {
+          item_title: '',
+          item_list: [] // array list
+        }
+      },
+
+      methods: {
+        addItem () 
+        {
+          this.item_list.push(this.item_title)
+          this.item_title = ''
+        },
+        delItem (n) {
+          // n is index of item list 
+          this.item_list.shift(n,1)
+        },
+        greet() {
+      // `this` inside methods points to the current active instance
+      alert(`Hello ${this.name}!`)
+      // `event` is the native DOM event
+      if (event) {
+        alert(event.target.tagName)
+      }
+    }
+      }
   }
-}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/List_Item.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>
+      {{ title }}
+    </h3>
+    <!-- for each add text add delete button  -->
+    <button @click="$emit('delete-item')">Delete</button> 
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title']
+}
+</script>
+<!-- scoped style jsut for this html area -->
+<style  scoped> 
+h3 {
+  display: inline-block;
+  margin: 10px;
+}
+</style>
